6a709faabfd0308cfaecc3732c413c545c62510a
mingquanjp <faker1652005@gmail.com>
2025-11-19 00:18:14 +0700

    feat : card component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-
+import Demo from "./components/commons/Card/test.js";
 function App() {
   return (
-    <div>La Khe Management
-      
-    </div>
-  )
+    <>
+      <div>La Khe Management</div>
+
+      <div style={{padding : '20px', margin : '0 4px auto'}}>
+        <Demo />
+      </div>
+    </>
+  );
 }
 
 export default App;
