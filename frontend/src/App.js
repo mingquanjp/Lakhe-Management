cb8ff804ee6c2421413dd2d2127da2c7ece01a8d
mingquanjp <faker1652005@gmail.com>
2025-11-21 00:48:00 +0700

    feat : admin population dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,8 @@
 import React from "react";
-import Header from "./components/layout/Header";
-import Sidebar from "./components/layout/Sidebar";
+import PopulationDashboard from "./pages/Admin/PopulationDashboard/PopulationDashboard";
 
 function App() {
-  return (
-    <div className="app-container">
-      <Sidebar />
-      <div className="main-content">
-        <Header />
-        <div className="content">
-          
-          <h1>Welcome to LaKhe Management</h1>
-        </div>
-      </div>
-    </div>
-  );
+  return <PopulationDashboard />;
 }
 
 export default App;
