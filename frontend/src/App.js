96394f83668bef8d981aee2d7a569c946b0f4be7
mingquanjp <faker1652005@gmail.com>
2025-11-20 20:49:44 +0700

    feat : core layout demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
+import React from "react";
+import Header from "./components/layout/Header";
+import Sidebar from "./components/layout/Sidebar";
 
 function App() {
   return (
-    <>
-      <div>La Khe Management</div>
-
-      <div>
-
+    <div className="app-container">
+      <Sidebar />
+      <div className="main-content">
+        <Header />
+        <div className="content">
+          
+          <h1>Welcome to LaKhe Management</h1>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
