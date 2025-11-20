a805c49b0ebb2084b1125359474d5214a4d4ac5a
Lightning <lightning1575@gmail.com>
2025-11-20 21:29:24 +0700

    update login frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import Header from "./components/layout/Header";
-import Sidebar from "./components/layout/Sidebar";
+import Login from "./components/Login";
+// import Header from "./components/layout/Header";
+// import Sidebar from "./components/layout/Sidebar";
 
 function App() {
   return (
-    <div className="app-container">
-      <Sidebar />
-      <div className="main-content">
-        <Header />
-        <div className="content">
-          
-          <h1>Welcome to LaKhe Management</h1>
-        </div>
-      </div>
-    </div>
+    <Login />
+    // <div className="app-container">
+    //   <Sidebar />
+    //   <div className="main-content">
+    //     <Header />
+    //     <div className="content">
+    //       
+    //       <h1>Welcome to LaKhe Management</h1>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
