1741e937bddb422f81fb960a1e043e248b99662f
Nguyen_Minh_Quan <nguyenminhquan11032005@gmail.com>
2025-11-21 09:43:48 +0700

    fetch : Create loading

--- frontend/src/components/commons/index.js
+++ frontend/src/components/commons/index.js
@@ -3,3 +3,4 @@ export { default as Button } from "./Button";
 export { default as Card } from "./Card";
 export { default as Table } from "./Table";
 export { default as Modal } from "./Modal";
+export { default as Loading } from "./Loading";
