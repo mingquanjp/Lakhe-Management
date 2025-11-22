bd39b790acc34205b2bebf931ccfa972ad95c17a
mingquanjp <faker1652005@gmail.com>
2025-11-22 15:08:08 +0700

    feat : Staff manegement page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PopulationDashboard from "./pages/Admin/PopulationDashboard/PopulationDashboard";
-
+import StaffManagement from "./pages/Admin/StaffManagement/StaffManagement";
 function App() {
-  return <PopulationDashboard />;
+  return <StaffManagement />;
 }
 
 export default App;
