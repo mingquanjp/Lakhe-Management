76179b7db5659b1fb04fee2dc12ea17c78f933a2
Lightning <lightning1575@gmail.com>
2025-11-22 23:57:50 +0700

    style: update app.js for mergecode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import Login from "./pages/Login";
-import Staff from "./pages/Staff";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [currentPage, setCurrentPage] = useState("dashboard");
-
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-    setCurrentPage("dashboard");
-  };
-
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-    setCurrentPage("dashboard");
-  };
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-
-  if (!isAuthenticated) {
-    return <Login onLogin={handleLogin} />;
-  }
-
-  return (
-    <Staff
-      currentPage={currentPage}
-      onPageChange={handlePageChange}
-      onLogout={handleLogout}
-    />
-  );
+  return <Login />;
 }
 
 export default App;
