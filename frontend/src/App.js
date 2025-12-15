d7f81e49b29d8b937b45507e134472d5ff80fbcf
Dinh Van Pham Viet <vietphamdinhvan@gmail.com>
2025-12-16 00:00:03 +0700

    Fix: conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -75,6 +75,7 @@ function AppRoutes() {
         <Route path="household/:id" element={<HouseholdDetail />} />
         <Route path="householdtemporary" element={<HouseholdTemporaryList />} />
         <Route path="householdtemporary/:id" element={<HouseholdDetail />} />     
+        
         <Route path="citizen" element={<Declaration />} />
         <Route path="form" element={<FormsMenu />} />
         <Route path="form/new-household-form" element={<NewHouseholdForm />} />
