d267e38de5294577698fd639df815b8a770b43bd
Dinh Van Pham Viet <vietphamdinhvan@gmail.com>
2025-12-27 03:31:49 +0700

    Fix: Stat-card in overview

--- frontend/src/pages/Admin/Overview/StatCard/StatCard.js
+++ frontend/src/pages/Admin/Overview/StatCard/StatCard.js
@@ -3,12 +3,12 @@ import './StatCard.css';
 
 const StatCard = ({ title, value}) => {
   return (
-    <div className="stat-card">
-      <div className="stat-header">
-        <span className="stat-title">{title}</span>
+    <div className="stat-card-overview">
+      <div className="stat-header-overview">
+        <span className="stat-title-overview">{title}</span>
       </div>
-      <div className="stat-content">
-        <h2 className="stat-value">{value}</h2>
+      <div className="stat-content-overview">
+        <h2 className="stat-value-overview">{value}</h2>
       </div>
     </div>
   );
